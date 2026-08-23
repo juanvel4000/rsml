267c1a1c1f2b16f3271b006d9a05cd523ff5d918
juanvel400 <contact@juanvel400.com>
2026-08-23 14:47:11 -0500

    validate the sender's email address in lmtp.py

--- src/rsml/lmtp.py
+++ src/rsml/lmtp.py
@@ -2,6 +2,7 @@
 
 from aiosmtpd.controller import Controller
 from aiosmtpd.lmtp import LMTP
+from email_validator import EmailNotValidError, validate_email
 
 from .config import RSMLConfig
 from .mailer import Mailer
@@ -22,22 +23,30 @@ class LMTPHandler:
     async def handle_RCPT(
         self, server, session, envelope, address, rcpt_options
     ) -> str:
+        """verify the sender"""
         if address.lower() != self.config.posting_email.lower():
             return "550 not relaying to that address"
 
         if not self.config.posting:
             return "550 5.7.1 delivery not authorized"
 
+        try:
+            email = validate_email(
+                envelope.mail_from, check_deliverability=False
+            ).normalized
+        except EmailNotValidError:
+            return "501 5.1.7 bad sender address syntax"
+
         if (
             self.config.posting_permissions == "subscribers"
-            and not self.storage.is_subscribed(envelope.mail_from)
+            and not self.storage.is_subscribed(email)
         ):
             return "550 5.7.1 delivery not authorized"
         envelope.rcpt_tos.append(address)
         return "250 OK"
 
     async def handle_DATA(self, server, session, envelope) -> str:
-
+        """store the message"""
         self.storage.store_message(envelope.content)
         await self.mailer.forward_message(
             envelope.content, self.storage.get_subscribers()
@@ -46,6 +55,7 @@ class LMTPHandler:
 
 
 def controller_init(config: RSMLConfig) -> LMTPController:
+    """initialize the lmtp controller"""
     storage = Storage(config)
     controller = LMTPController(
         LMTPHandler(config, storage), hostname=config.lmtp_host, port=config.lmtp_port
